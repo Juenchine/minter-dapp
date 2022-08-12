9eb6f1506c532ce3cfde5f29d5b757e146bf1be6
Julien Reboutier <julienreboutier@macbook-pro-de-julien.home>
2022-08-12 16:29:01 +0200

    rinkebe test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x23fE16730c2407774b7872699975808250Df2467"; // Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
